1bebffa03676803e686dfbede537fc0a961d2238
Hieu Le <hieu.l@kbtgvn.tech>
2023-09-19 23:19:00 +0700

    Initial commit

---
+++ apps/hieu-next-kflix-e2e/src/e2e/app.cy.ts
@@ -0,0 +1,13 @@
+import { getGreeting } from '../support/app.po';
+
+describe('hieu-next-kflix', () => {
+  beforeEach(() => cy.visit('/'));
+
+  it('should display welcome message', () => {
+    // Custom command example, see `../support/commands.ts` file
+    cy.login('my-email@something.com', 'myPassword');
+
+    // Function helper example, see `../support/app.po.ts` file
+    getGreeting().contains('Welcome hieu-next-kflix');
+  });
+});
